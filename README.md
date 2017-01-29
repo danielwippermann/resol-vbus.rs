63614399a4ad3da08eafac48069179bb58e2eac8
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2017-01-29 10:59:58 +0100

    Import project skeleton.

---
+++ README.md
@@ -0,0 +1,8 @@
+# rust-resol-vbus
+
+A Rust library for processing RESOL VBus data.
+
+
+## Features
+
+- TBD
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+// This is part of rust-resol-vbus.
+// Copyright (c) 2017, Daniel Wippermann.
+// See README.md and LICENSE.txt for details.
+
+//! # rust-resol-vbus
+//!
+//! A Rust library for processing RESOL VBus data.
+//!
+//! ## Features
+//!
+//! - TBD
+
+#![warn(missing_docs)]
+#![deny(missing_debug_implementations)]
+// #![deny(warnings)]
+
+extern crate byteorder;
+extern crate chrono;
