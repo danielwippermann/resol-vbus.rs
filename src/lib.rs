b28a8349e350e1a89f649a97bd57c78ca772300f
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2017-02-19 07:56:54 +0100

    Update the example in the crate-level documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,10 @@
 //!
 //! ## Examples
 //!
+//! ### Converter for recorded VBus data to CSV.
+//!
 //! ```rust
+//! //! A converter from the binary recorded VBus file format to human-readable CSV.
 //! extern crate chrono;
 //! extern crate resol_vbus;
 //!
@@ -57,7 +60,7 @@
 //! use resol_vbus::*;
 //!
 //!
-//! // Load the VSF file to allow decoding of the payload contained in `Packet` `frame_data` values.
+//! /// Load the VSF file to allow decoding of the payload contained in `Packet` `frame_data` values.
 //! fn load_vsf_file(vsf_filename: &str) -> Specification {
 //!     let mut f = File::open(vsf_filename).unwrap();
 //!
@@ -72,33 +75,13 @@
 //! }
 //!
 //!
-//! fn main() {
-//!     let vsf_filename = "vbus_specification.vsf";
-//!
-//!     let file_list = std::env::args().take(1).map(|arg| arg.to_owned()).collect::<Vec<_>>();
-//!
-//!     //
-//!     // Load the VSF file to allow decoding of `Packet` values.
-//!     //
-//!     let spec = load_vsf_file(vsf_filename);
-//!
-//!     //
-//!     // Read the "*.vbus" files once to find all unique `Packet` values. This allows to
-//!     // only generate CSV column headers once and keep the column layout stable throughout
-//!     // the conversion.
-//!     //
-//!     let flr = FileListReader::new(file_list.clone());
-//!
-//!     let mut rr = LiveDataRecordingReader::new(flr);
-//!
-//!     let topology_data_set = rr.read_topology_data_set().unwrap();
-//!
-//!     //
-//!     // Read the "*.vbus" files a second time to convert / process the `DataSet` values within.
-//!     //
+//! /// Read the "*.vbus" files a second time to convert / process the `DataSet` values within.
+//! fn print_data(file_list: Vec<String>, topology_data_set: DataSet, spec: &Specification) {
 //!     let flr = FileListReader::new(file_list);
 //!
 //!     let mut rr = LiveDataRecordingReader::new(flr);
+//!
+//!     let mut cumultative_data_set = topology_data_set;
 //!
 //!     let mut output = std::io::stdout();
 //!
@@ -106,14 +89,41 @@
 //!         let timestamp = data.as_ref().timestamp;
 //!         let local_timestamp = timestamp.with_timezone(&Local);
 //!
+//!         cumultative_data_set.add_data(data);
+//!
 //!         write!(output, "{}", local_timestamp).unwrap();
 //!
-//!         for field in spec.fields_in_data_set(&[ data ]) {
+//!         for field in spec.fields_in_data_set(&cumultative_data_set.as_ref()) {
 //!             write!(output, "\t{}", field.fmt_raw_value(false)).unwrap();
 //!         }
 //!
 //!         write!(output, "\n").unwrap();
 //!     }
+//! }
+//!
+//!
+//! fn main() {
+//!     let vsf_filename = "vbus_specification.vsf";
+//!
+//!     let file_list = std::env::args().skip(1).map(|arg| arg.to_owned()).collect::<Vec<_>>();
+//!
+//!     // Load the VSF file to allow decoding of `Packet` values.
+//!     let spec = load_vsf_file(vsf_filename);
+//!
+//!     // Read the "*.vbus" files once to find all unique `Packet` values. This allows to
+//!     // only generate CSV column headers once and keep the column layout stable throughout
+//!     // the conversion.
+//!     let flr = FileListReader::new(file_list.clone());
+//!
+//!     let mut rr = LiveDataRecordingReader::new(flr);
+//!
+//!     let topology_data_set = rr.read_topology_data_set().unwrap();
+//!     for data in topology_data_set.as_data_slice() {
+//!         println!("{}: {:?}", data.id_string(), data);
+//!     }
+//!
+//!     // Read the "*.vbus" files a second time to convert / process the `DataSet` values within.
+//!     print_data(file_list, topology_data_set, &spec);
 //! }
 //! ```
 
