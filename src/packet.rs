69d396af7d8b9a88f35df67407f9f8ef5209445e
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2017-03-19 17:46:40 +0100

    Add `PacketId` and `PacketFieldId` types.

--- src/packet.rs
+++ src/packet.rs
@@ -5,6 +5,220 @@ use id_hash::IdHash;
 use header::Header;
 
 
+/// A tuple of identification information about a `Packet` value.
+///
+/// It consists of the following parts:
+///
+/// - the channel
+/// - the destination address
+/// - the source address
+/// - the command
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct PacketId(pub u8, pub u16, pub u16, pub u16);
+
+
+impl PacketId {
+
+    /// Create an ID string for the given `PacketId` value.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use resol_vbus::PacketId;
+    ///
+    /// assert_eq!("11_1213_1415_10_1718", PacketId(0x11, 0x1213, 0x1415, 0x1718).packet_id_string());
+    /// ```
+    pub fn packet_id_string(&self) -> String {
+        format!("{:02X}_{:04X}_{:04X}_10_{:04X}", self.0, self.1, self.2, self.3)
+    }
+
+}
+
+
+/// A trait to get a `PacketId` for a given value.
+pub trait ToPacketId {
+
+    /// Get the `PacketId` for a given value.
+    fn to_packet_id(&self) -> Result<PacketId, String>;
+
+}
+
+
+impl ToPacketId for PacketId {
+
+    fn to_packet_id(&self) -> Result<PacketId, String> {
+        Ok(*self)
+    }
+
+}
+
+
+impl ToPacketId for str {
+
+    /// Parse the string into a packet ID tuple.
+    ///
+    /// ## Examples
+    ///
+    /// ```rust
+    /// use resol_vbus::{PacketId, ToPacketId};
+    ///
+    /// assert_eq!(PacketId(0x11, 0x1213, 0x1415, 0x1718), "11_1213_1415_10_1718".to_packet_id().unwrap());
+    /// ```
+    fn to_packet_id(&self) -> Result<PacketId, String> {
+        let is_not_hex_char = |c| {
+            match c {
+                '0'...'9' | 'A'...'F' | 'a'...'f' => false,
+                _ => true,
+            }
+        };
+
+        if self.len() < 20 {
+            return Err(format!("Invalid length of input {:?}", self));
+        }
+
+        let mut parts = self.split('_');
+
+        let channel_str = parts.next().unwrap();
+        if channel_str.len() != 2 {
+            return Err(format!("Invalid length of channel {:?}", channel_str));
+        }
+        if channel_str.chars().any(&is_not_hex_char) {
+            return Err(format!("Invalid characters in channel {:?}", channel_str));
+        }
+        let channel = u8::from_str_radix(channel_str, 16).unwrap();
+
+        let destination_address_str = parts.next().unwrap();
+        if destination_address_str.len() != 4 {
+            return Err(format!("Invalid length of destination address {:?}", destination_address_str));
+        }
+        if destination_address_str.chars().any(&is_not_hex_char) {
+            return Err(format!("Invalid characters in destination address {:?}", destination_address_str));
+        }
+        let destination_address = u16::from_str_radix(destination_address_str, 16).unwrap();
+
+        let source_address_str = parts.next().unwrap();
+        if source_address_str.len() != 4 {
+            return Err(format!("Invalid length of source address {:?}", source_address_str));
+        }
+        if source_address_str.chars().any(&is_not_hex_char) {
+            return Err(format!("Invalid characters in source address {:?}", source_address_str));
+        }
+        let source_address = u16::from_str_radix(source_address_str, 16).unwrap();
+
+        let protocol_version_str = parts.next().unwrap();
+        if protocol_version_str.len() != 2 {
+            return Err(format!("Invalid length of protocol version {:?}", protocol_version_str));
+        }
+        if protocol_version_str.chars().any(&is_not_hex_char) {
+            return Err(format!("Invalid characters in protocol version {:?}", protocol_version_str));
+        }
+        let protocol_version = u8::from_str_radix(protocol_version_str, 16).unwrap();
+        if (protocol_version & 0xF0) != 0x10 {
+            return Err(format!("Unsupported protocol version 0x{:02X}", protocol_version));
+        }
+
+        let command_str = parts.next().unwrap();
+        if command_str.len() != 4 {
+            return Err(format!("Invalid length of command {:?}", command_str));
+        }
+        if command_str.chars().any(&is_not_hex_char) {
+            return Err(format!("Invalid characters in command {:?}", command_str));
+        }
+        let command = u16::from_str_radix(command_str, 16).unwrap();
+
+        Ok(PacketId(channel, destination_address, source_address, command))
+    }
+
+}
+
+
+/// A tuple of identification information about a field in a `Packet` value.
+///
+/// It consists of the following parts:
+///
+/// - the packet ID tuple (channel, destination address, source address and command)
+/// - the field ID
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct PacketFieldId<'a>(pub PacketId, pub &'a str);
+
+
+impl<'a> PacketFieldId<'a> {
+
+    /// Get the packet ID string for a given `PacketFieldId` value.
+    ///
+    /// ## Examples
+    ///
+    /// ```rust
+    /// use resol_vbus::{PacketId, PacketFieldId};
+    ///
+    /// let packet_field_id = PacketFieldId(PacketId(0x11, 0x1213, 0x1415, 0x1718), "012_4_0");
+    /// assert_eq!("11_1213_1415_10_1718", packet_field_id.packet_id_string());
+    /// ```
+    pub fn packet_id_string(&self) -> String {
+        self.0.packet_id_string()
+    }
+
+    /// Get the packet field ID string for a given `PacketFieldId` value.
+    ///
+    /// ## Examples
+    ///
+    /// ```rust
+    /// use resol_vbus::{PacketId, PacketFieldId};
+    ///
+    /// let packet_field_id = PacketFieldId(PacketId(0x11, 0x1213, 0x1415, 0x1718), "012_4_0");
+    /// assert_eq!("11_1213_1415_10_1718_012_4_0", packet_field_id.packet_field_id_string());
+    /// ```
+    pub fn packet_field_id_string(&self) -> String {
+        format!("{}_{}", self.packet_id_string(), self.1)
+    }
+
+}
+
+
+/// A trait to get a `PacketFieldId` for a given value.
+pub trait ToPacketFieldId {
+
+    /// Get the `PacketFieldId` for a given value.
+    fn to_packet_field_id(&self) -> Result<PacketFieldId, String>;
+
+}
+
+
+impl<'a> ToPacketFieldId for PacketFieldId<'a> {
+
+    fn to_packet_field_id(&self) -> Result<PacketFieldId, String> {
+        Ok(*self)
+    }
+
+}
+
+
+impl ToPacketFieldId for str {
+
+    /// Parse the string into a packet field ID tuple.
+    ///
+    /// ## Examples
+    ///
+    /// ```rust
+    /// use resol_vbus::{PacketId, PacketFieldId, ToPacketFieldId};
+    ///
+    /// assert_eq!(PacketFieldId(PacketId(0x11, 0x1213, 0x1415, 0x1718), "012_4_0"), "11_1213_1415_10_1718_012_4_0".to_packet_field_id().unwrap());
+    /// ```
+    fn to_packet_field_id(&self) -> Result<PacketFieldId, String> {
+        if self.len() < 21 {
+            return Err(format!("Invalid length of input {:?}", self));
+        }
+
+        let packet_id = self.to_packet_id()?;
+
+        let field_id = &self [21..];
+
+        Ok(PacketFieldId(packet_id, field_id))
+    }
+
+}
+
+
 /// The `Packet` type stores information according to the VBus protocol version 1.x.
 ///
 /// Packets are used to transmit larger amount of information (up to 508 bytes of payload) relying
