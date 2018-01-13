b92b092d2a7c0b990355aa4ed8c09e424be1439e
Daniel Wippermann <Daniel.Wippermann@gmail.com>
2018-01-13 09:11:48 +0100

    Add escaping in simple JSON formatter.

--- examples/formatter/src/simple_json_generator.rs
+++ examples/formatter/src/simple_json_generator.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use std::io::Write;
 
 use resol_vbus::chrono::{Local};
@@ -6,6 +7,35 @@ use app_error::Result;
 use config::Config;
 use field_iterator::*;
 use timestamp_file_writer::TimestampFileWriter;
+
+
+struct JsonEscape<'a> {
+    input: &'a str,
+}
+
+
+impl<'a> JsonEscape<'a> {
+
+    pub fn new(input: &'a str) -> JsonEscape<'a> {
+        JsonEscape{ input: input }
+    }
+
+}
+
+
+impl<'a> Display for JsonEscape<'a> {
+
+    fn fmt(&self, f: &mut Formatter) -> ::std::result::Result<(), ::std::fmt::Error> {
+        for c in self.input.chars() {
+            match c {
+                '\\' | '"' => write!(f, "\\{}", c)?,
+                _ => write!(f, "{}", c)?,
+            }
+        }
+        Ok(())
+    }
+
+}
 
 
 pub fn generate(config: &mut Config) -> Result<()> {
@@ -34,12 +64,12 @@ pub fn generate(config: &mut Config) -> Result<()> {
             }
             write!(output, "{{\n")?;
             write!(output, "        \"id\": \"{}_{}\",\n", field.packet_spec().packet_id, field.field_id())?;
-            write!(output, "        \"packetName\": \"{}\",\n", field.packet_spec().name)?;
-            write!(output, "        \"fieldName\": \"{}\",\n", field.field_spec().name)?;
+            write!(output, "        \"packetName\": \"{}\",\n", JsonEscape::new(&field.packet_spec().name))?;
+            write!(output, "        \"fieldName\": \"{}\",\n", JsonEscape::new(&field.field_spec().name))?;
             write!(output, "        \"rawValue\": \"{}\",\n", field.raw_value_i64().unwrap())?;
             write!(output, "        \"textValue\": \"{}\",\n", field.fmt_raw_value(false))?;
-            write!(output, "        \"unitCode\": \"{}\"\n", field.field_spec().unit_code)?;
-            write!(output, "        \"unitText\": \"{}\"\n", field.field_spec().unit_text)?;
+            write!(output, "        \"unitCode\": \"{}\"\n", JsonEscape::new(&field.field_spec().unit_code))?;
+            write!(output, "        \"unitText\": \"{}\"\n", JsonEscape::new(&field.field_spec().unit_text))?;
             write!(output, "    }}")?;
         }
         write!(output, "]\n")?;
